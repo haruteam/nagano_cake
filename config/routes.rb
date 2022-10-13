d6b0e1da836f80d0407c57c873f02803a7a28d16
harukikawakita3 <113086279+harukikawakita3@users.noreply.github.com>
2022-10-13 02:02:30 +0000

    finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,67 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-  end
-  namespace :public do
-    get 'orders/new'
-    get 'orders/confirm'
-    get 'orders/complete'
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'cart_items/index'
-  end
-  namespace :public do
-    get 'cart_items/index'
-  end
-  namespace :admin do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  namespace :public do
-    get 'addresses/index'
-    get 'addresses/edit'
-  end
-  get 'addresses/index'
-  get 'addresses/edit'
   root to: "public/homes#top"
   get "home/about"=>"public/homes#about",as: "about"
-
+  # public items
+  resources :items, only: [:index, :show]
+  
+  # 顧客登録,login
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-
+  
+  # customers ★
+  resource :customers, only: [:show, :edit, :update]
+  get 'customers/confirm'
+  put '/customers'=>'customers#withdrow'
+  
+  # cart_items
+  resources :cart_items, only: [:index, :create, :update, :destroy]
+  delete 'cart_items/destroy_all'
+  
+  # orders
+  resources :orders, only: [:new, :create, :show, :index]
+  get 'orders/confirm'
+  get 'orders/complete'
+  
+  # deliveries
+  resources :deliveries, only: [:index, :edit, :create, :update, :destroy]
+  
+  # admin login~
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
    sessions: "admin/sessions"
   }
+  
+  # admin items
+  namespace :admin do
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  end
+  
+  # admin genres
+  namespace :admin do
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+  # admin cutomers
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit, :update]
+  end
+  
+  # admin orders
+  namespace :admin do
+    resources :orders, only: [:update, :show]
+  end
+  
+  # admon order_detail
+  namespace :admin do
+    resources :order_details, only: [:update]
+  end
+  
+  
+ 
+  
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
