ad42f819b9163d60d662ec932fd0f1ea4a1e4de1
harukikawakita3 <113086279+harukikawakita3@users.noreply.github.com>
2022-10-18 19:35:06 +0900

    feat enum

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
   belongs_to :genre
+
+  enum is_active: {販売中: true, 販売停止中: false}
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ApplicationRecord
+  enum production_status: {cant: 0, wait: 1, production:2, finish:3}
+  
 end
